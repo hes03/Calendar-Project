77d1a19b443f0a5564f19087baf4502ce7ffb015
dmstn <eunsoo4629@naver.com>
2026-01-13 09:38:10 +0900

    프로젝트 시작

---
+++ vite-step1/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./components/pages/Homepage"
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ vite-step1/src/components/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>
+      {/* <Header /> */}
+      <div>HomePage</div>
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default Homepage
